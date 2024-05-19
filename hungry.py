1215859b874b23127243ba43284e8f7add5d0a2e
laptop_mc2 <malayaja.chutani@gmail.com>
2024-05-19 18:18:29 +0530

    apple

--- hungry.py
+++ hungry.py
@@ -4,6 +4,7 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
     print("eat fries")
+    print("eat an apple too")
 else:
     thirsty = input("are you thirsty?")
     if thirsty=="yes":
